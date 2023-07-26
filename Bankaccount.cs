35a2a160cba70f8d703d3439a59bbbb59db5cb62
sand2104 <prasadsandy2000@gmail.com>
2023-07-26 16:33:34 +0530

    first commit

---
+++ Bankaccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment16
+{
+    internal class Bankaccount
+    {
+        private static int nextAccountNumber = 1;
+        private readonly int accountNumber;
+        public int AccountNumber
+        {
+            get { return accountNumber; }
+        }
+        private string accountHolderName;
+        public string AccountHolderName
+        {
+            get { return accountHolderName; }
+            set { accountHolderName = value; }
+        }
+        private double balance;
+        public Bankaccount(string accountHolderName)
+        {
+            this.accountNumber = nextAccountNumber++;
+            this.accountHolderName = accountHolderName;
+            this.balance = 0.0;
+        }
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit must be greater than zero");
+            }
+            balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdrawl amount must be greater than zero");
+            }
+            if (amount > balance)
+            {
+                throw new ArgumentException("Insufficient balance");
+            }
+            balance -= amount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter Account Holder Name");
+                    string accountHolderName = Console.ReadLine();
+                    Bankaccount account = new Bankaccount(accountHolderName);
+                    Console.WriteLine($"Account Number: {account.AccountNumber}");
+                    Console.WriteLine($"Account Holder Name: {account.AccountHolderName}");
+                    Console.WriteLine($"Account Balance: {account.GetBalance()}");
+
+                    Console.WriteLine("Enter the amount to be deposited");
+                    double depositAmount = double.Parse(Console.ReadLine());
+                    account.Deposit(depositAmount);
+                    Console.WriteLine($"Balance after deposit: {account.GetBalance()}");
+
+                    Console.WriteLine("Enter the amount to be withdrwan");
+                    double withdrawAmount = double.Parse(Console.ReadLine());
+                    account.Withdraw(withdrawAmount);
+                    Console.WriteLine($"Balance after withdrawl: {account.GetBalance()}");
+
+                    Console.WriteLine("Do you want to continue? (Y/N)");
+                    string response = Console.ReadLine().ToUpper();
+                    if (response != "Y")
+                    {
+                        break;
+                    }
+                    Console.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
